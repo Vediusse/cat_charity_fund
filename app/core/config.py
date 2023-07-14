4f1fd552d42ebd6052d2def5190975d87aae4f7c
Valerii Rublev <vedius.rublev@gmail.com>
2023-07-15 00:07:19 +0300

    sadf

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings, EmailStr
 
 
 class Settings(BaseSettings):
-    app_title: str = "Бронирование переговорок"
+    app_title: str = "QrKot"
     database_url: str = "sqlite+aiosqlite:///./fastapi.db"
     secret: str = "secret"
     first_superuser_email: Optional[EmailStr] = None
